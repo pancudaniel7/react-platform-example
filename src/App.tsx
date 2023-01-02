8820d70a816a96efc241ac2d5f799dbbc6138696
Daniel <pancudaniel7@gmail.com>
2023-01-02 23:10:38 +0200

    Implemented NotFound and Ticket pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-
+import NotFound from './pages/not-found/NotFound';
+import Ticket from './pages/ticket/Ticket';
 
 function App() {
   return (
-    <div className='App'>
-      <p>Work in progress</p>
-            
-    </div>
+    // <div className='App'>
+    //   <p>Work in progress</p>
+
+    // </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Ticket />}/>
+        <Route path='/ticket' element={<Ticket />}/>
+        <Route path='*' element={<NotFound />}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/not-found/NotFound.tsx
@@ -0,0 +1,11 @@
+import './NotFound.css';
+
+function NotFound() {
+  return (
+    <div className="not-found">
+     Not found!
+    </div>
+  );
+}
+
+export default NotFound;
