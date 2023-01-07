9d1a09db9206be7e87a16b3fa07d0ecfabc2130f
Daniel Jecu <catalin.jecu@gmail.com>
2023-01-07 18:04:41 +0200

    update notfound as index.tsx

---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,11 @@
+import "./NotFound.css";
+
+function NotFound() {
+  return (
+    <div className="not-found">
+      <h1 className="not-found-text">Not found!</h1>
+    </div>
+  );
+}
+
+export default NotFound;
